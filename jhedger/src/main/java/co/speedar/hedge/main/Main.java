36d60eb72b7ad6c41fef6132df0d017494d12598
lixuanbin <lxbjkben@163.com>
2015-10-12 19:43:09 +0800

    java service wrapper added;

--- jhedger/src/main/java/co/speedar/hedge/main/Main.java
+++ jhedger/src/main/java/co/speedar/hedge/main/Main.java
@@ -2,9 +2,13 @@ package co.speedar.hedge.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.log4j.Logger;
 
 public class Main {
+	protected static final Logger log = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		log.info("started...");
 	}
 }
